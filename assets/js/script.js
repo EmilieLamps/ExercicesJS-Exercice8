7c8119c7f2e772bf868c9eb49b76939d9e5e32a5
EmilieLamps <emilie.lamps@gmail.com>
2019-10-28 14:15:06 +0100

    Correction de l'exercice

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,12 @@ let age = document.getElementById("age").value; // récupérer les variables
 if (age >=18) { //conditions
   alert("Vous êtes majeur"); //Ne pas oublier les guillemets dans la parenthèse alert
   }
+else if (age >=0) {
+  alert("vous êtes mineur");
+}
 else {
-  alert("Vous êtes mineur");
+  alert("Saisie invalide");
   }
 }
+// Vérifier les tranches d'âge avec else if
+// Voir la correction dans le dossier de l'exercice
